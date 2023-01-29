4239b58daf36dbdf67aa1a046ae832ac5494b085
Mansur Fayziev <114176073+mansurfayziev@users.noreply.github.com>
2023-01-29 11:36:23 +0500

    Add files via upload

---
+++ backend_api/views.py
@@ -0,0 +1,20 @@
+from rest_framework.views import APIView
+from .models import YouTubeVideo
+from .serializer import YouTubeVideoSerializer
+from rest_framework.response import Response
+
+class YouTubeVideoView(APIView):
+    def get(self,request):
+        output=[
+            {
+                "title":output.title,
+                "channel":output.channel
+            }for output in YouTubeVideo.objects.all()
+        ]
+        return Response(output)
+
+    def post(self,request):
+        serializer=YouTubeVideoSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
